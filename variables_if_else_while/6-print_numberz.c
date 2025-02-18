72163a1cf681d1dd45c2c20fbd8258d0fcfd26f7
BenoitMain <benoitmaingon@gmail.com>
2025-02-18 15:23:25 +0100

    add_6-print_numberz.c

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+* main -Write a program that prints all single digit numbers of base 1
+* starting from 0,followed by a new line.
+*
+* Return: 0
+*/
+int main(void)
+{
+	int Single_Digit = '0';
+
+	while (Single_Digit <= '9')
+	{
+		putchar (Single_Digit++);
+	}
+	putchar('\n');
+	return (0);
+}
