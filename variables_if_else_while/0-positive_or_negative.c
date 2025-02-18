a234ee8a0215995da614eea8d9355dbe76aadf32
BenoitMain <benoitmaingon@gmail.com>
2025-02-18 09:25:42 +0100

    Better_radabilityV1

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,10 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /**
 * main - Print number random
 *
-* Return : 0
+* Return: 0
 */
 int main(void)
 {
@@ -12,6 +13,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
 	if (n > 0)
 	{
 	printf("%d is positive\n", n);
