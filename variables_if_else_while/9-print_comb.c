c218e3496a54a796427ccb4d452ba5b2e75f33af
BenoitMain <benoitmaingon@gmail.com>
2025-02-18 17:02:37 +0100

    add_if_command

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -9,14 +9,16 @@ int main(void)
 {
 	int Single_Digit = '0';
 
-	while (Single_Digit < '9')
+	while (Single_Digit <= '9')
 	{
-		putchar (Single_Digit++);
+		putchar (Single_Digit);
+		if (Single_Digit != '9')
+		{
 		putchar (',');
 		putchar (' ');
-		if (Single_Digit == '9')
-			putchar ('9');
+		}
 	}
+	Single_Digit++;
 	putchar('\n');
 	return (0);
 }
