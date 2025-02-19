fa67191b7f1b0f522600f40606e11be9074afbfe
BenoitMain <benoitmaingon@gmail.com>
2025-02-19 13:53:16 +0100

    add_1-main.c_1-alphabet.c_and_prototype_voirprint

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* print_alphabet - Write a function that prints the alphabet, in lowercase,
+* followed by a new line.
+*
+* Return: VOID
+*/
+void print_alphabet(void)
+{
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		_putchar(a++);
+	}	
+	_putchar('\n');
+}
