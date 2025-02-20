76464c92a3bd3cc25dd006b84abb8fd4b60a2135
root <benoitmaingon.@gmail.com>
2025-02-20 11:00:45 +0000

    add_.gitignore_6-abs.c

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *
+ * int_abs - Write a function that computes the absolute value of an integer.
+ * @r: The character to check
+ *
+ * Return: -n or n
+ */
+int _abs(int n)
+{
+	if ( n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}	
