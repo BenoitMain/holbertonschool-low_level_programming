a2ebc0f5c5adb6ba0957e7694fb9091fcc7256bf
root <benoitmaingon.@gmail.com>
2025-02-20 11:07:17 +0000

    replace_word_in_comment

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,15 +1,14 @@
 #include "main.h"
 
 /**
- *
- * int_abs - Write a function that computes the absolute value of an integer.
- * @r: The character to check
+ * _abs - Write a function that computes the absolute value of an integer.
+ * @n: The character to check
  *
  * Return: -n or n
  */
 int _abs(int n)
 {
-	if ( n < 0)
+	if (n < 0)
 	{
 		return (-n);
 	}
@@ -17,4 +16,4 @@ int _abs(int n)
 	{
 		return (n);
 	}
-}	
+}
