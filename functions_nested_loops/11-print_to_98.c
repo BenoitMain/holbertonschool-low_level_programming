2fd61fbce862151c29c1eb10f0caf6b7491a5051
BenoitMain <benoitmaingon@gmail.com>
2025-02-21 12:52:31 +0100

    add_comment_for_betty

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,9 +1,11 @@
 #include "main.h"
 #include<stdio.h>
 /**
+ * print_to_98 - Write a function that prints all natural numbers from n to 98,
+ * followed by a new line.
+ * @n: First number
  *
- *
- *
+ * Return: VOID
  */
 void print_to_98(int n)
 {
