e18ab004fa26246571eea01b83238cbbf98bddcc
BenoitMain <benoitmaingon@gmail.com>
2025-02-21 10:42:21 +0100

    add_10-main.c_10-add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* add - Write a function that adds two integers and returns the result.
+* @a: First integer
+* @b: Second integer
+*
+* Return: Sum a and b
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
