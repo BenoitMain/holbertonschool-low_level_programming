ab55010244703c2e85fc92dd8eabb6b5c9a191de
BenoitMain <benoitmaingon@gmail.com>
2025-02-25 16:29:46 +0100

    add_2-main.c_2-strlen.c

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen - Write a function that returns the length of a string.
+ *@s: Pointerto check.
+ *
+ * Return: 0.
+ */
+int _strlen(char *s)
+{
+int i;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+}
+return (i);
+}
+
