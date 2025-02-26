316bcad0161e9502db183fbf252ac1e55f00cd67
BenoitMain <benoitmaingon@gmail.com>
2025-02-26 14:33:06 +0100

    add_7-main.c_7-puts_half.c

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ *
+ * puts_half - Write a function that prints half of a string,
+ * followed by a new line.
+ * @str: pointer to string.
+ *
+ * Retunr: VOID
+ */
+void puts_half(char *str)
+{
+	int n, length, mid;
+
+	length = 0;
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+
+	if (length % 2 == 0)
+	{
+		mid = length / 2;
+		for (n = mid; n < length; n++)
+		{
+			_putchar(str[n]);
+		}
+	}
+	else
+	{
+		mid = (length - 1) / 2;
+		for (n = mid + 1; n < length; n++)
+		{
+			_putchar(str[n]);
+		}
+	}
+	_putchar('\n');
+}
