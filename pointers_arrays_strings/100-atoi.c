9f167a39659f8101d330dc34d3bbd350217e6396
BenoitMain <benoitmaingon@gmail.com>
2025-03-01 21:15:30 +0100

    add_100-main.c_100-atoi.c

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,9 +1,42 @@
 #include "main.h"
 /**
+ * _atoi - ASCII To Integer.
+ * @c: Le caractère à vérifier
  *
- *
- *
+ * Return: 1 si c est un chiffre, 0 sinon
  */
-int _atoi(char *s);
+int digit(char c)
 {
-	int 
+	return (c >= '0' && c <= '9');
+}
+int _atoi(char *s)
+{
+    int res = 0;
+    int sign = 1;
+    int digit_found = 0;
+
+	while (*s)
+	{
+		if (digit(*s))
+		{
+			digit_found = 1;
+
+			if (res > 214748364 || (res == 214748364 && (*s - '0') > 7))
+			{
+				return (sign == 1) ? 2147483647 : -2147483648;
+			}	
+			res = res * 10 + (*s - '0');
+		}
+			else if (*s == '-' && !digit_found)
+			{
+				sign *= -1;
+			}
+			else if (*s != '+' && digit_found)
+			{
+				break;
+			}
+		s++;
+	}
+	return res * sign;
+}
+
