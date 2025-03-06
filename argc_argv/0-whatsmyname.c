1a60efbd0ef34a3fc23e8d6fc18e411dfd2fbaf6
BenoitMain <benoitmaingon@gmail.com>
2025-03-06 17:51:14 +0100

    add_README_0-whatsmyname.c

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *
+ *
+ *
+ *
+ */
+int main(char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argv; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
