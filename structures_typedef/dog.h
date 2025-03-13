fcf4e8a936c04fe50bbe4061541c94ce00b99c07
BenoitMain <benoitmaingon@gmail.com>
2025-03-13 09:00:12 +0100

    Add_0-main.c_dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Defines a dog's basic information.
+ * @name: First member - dog's name.
+ * @age: Second member - dog's age.
+ * @owner: Third member - owner's name.
+ *
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* DOG_H */
