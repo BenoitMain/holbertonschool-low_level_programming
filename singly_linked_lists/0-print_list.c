7e1d5deb883b5e7e3b568fe2c3e422b6aedabf96
BenoitMain <benoitmaingon@gmail.com>
2025-03-31 11:26:28 +0200

    Add_betty_comment

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -3,11 +3,10 @@
 #include <string.h>
 #include <stdio.h>
 /**
+ * print_list - Prints all the elements of a linked list.
+ * @h: Pointer to the list_t list to be printed.
  *
- *
- *
- *
- *
+ * Return: The number of nodes in the list.
  */
 size_t print_list(const list_t *h)
 {
