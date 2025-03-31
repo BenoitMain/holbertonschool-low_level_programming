cfd42bc994bdb84515d128b5020b56cbdd0ea075
BenoitMain <benoitmaingon@gmail.com>
2025-03-31 17:10:08 +0200

    Add constructor function to print a message before main execution
    - Created my_constructor function using __attribute__((constructor)) to
      print a predefined message before main is executed.
    - Ensured the main function remains in 100-main.c.

---
+++ singly_linked_lists/100-first.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * my_constructor - Prints a message before main is executed.
+ *
+ * This function uses the constructor attribute to print a predefined
+ * message before the main function starts. The message will be displayed
+ * when the program is initialized.
+ *
+ * Return: None.
+ */
+void my_constructor(void) __attribute__((constructor));
+
+void my_constructor(void)
+{
+printf("You're beat! and yet, you must allow,\n
+	I bore my house upon my back!\n");
+}
+
