b930df2754dd7fea422e78e292c249797814ce7e
BenoitMain <benoitmaingon@gmail.com>
2025-03-31 16:57:14 +0200

    feat: Implement free_list function to free a linked list
    - Added the `free_list(list_t *head)` function.
    - Frees each node in the list, starting with its dynamically allocated string (`str`).
    - Ensures proper pointer advancement before freeing the current node.
    - Prevents memory leaks by correctly deallocating all dynamically allocated elements.
    
    Tested with Valgrind: no memory leaks detected.

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -3,11 +3,20 @@
 #include <stdio.h>
 #include "lists.h"
 /**
+ * free_list - Free linked list.
+ * @head: Pointer to first element.
  *
- *
- *
+ * Return: VOID.
  */
 void free_list(list_t *head)
 {
-	
+	list_t *free_node;
+
+	while (head != NULL)
+	{
+		free_node = head;
+		head = head->next;
+		free(free_node->str);
+		free(free_node);
+	}
 }
