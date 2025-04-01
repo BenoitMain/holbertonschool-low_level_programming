4737c9b8a27a947a4b80e76d9bd46b79ff060021
BenoitMain <benoitmaingon@gmail.com>
2025-04-01 16:10:07 +0200

    Add betty comment

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -3,10 +3,8 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- *
- *
- *
- *
+ * free_dlistint - Frees a dlistint_t list.
+ * @head: Pointer to the head of the list.
  */
 void free_dlistint(dlistint_t *head)
 {
