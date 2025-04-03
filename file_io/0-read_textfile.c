d06bb83930bdee99b5ecca07b54ac8b378d344a0
BenoitMain <benoitmaingon@gmail.com>
2025-04-03 11:20:03 +0200

    feat: Implement function to read and print a text file Added the read_textfile function that reads a specified number of characters from a given text file and prints them to the standard output. The function handles errors such as invalid file names, failed memory allocation, or failed read/write operations.

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include "main.h"
+/**
+ * read_textfile - Reads a text file and prints it to the POSIX standard output.
+ * @filename: Pointer to the name of the file to be read.
+ * @letters: Number of letters to read and print.
+ *
+ * Return: The actual number of letters it could read and print,
+ *         or 0 if the file cannot be opened, read, or memory allocation fails.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buffer;
+	int fd;
+ssize_t var_write, var_read;
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		return (0);
+	}
+
+	buffer = malloc(letters);
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+	var_read = read(fd, buffer, letters);
+	if (var_read == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+	var_write = write(1, buffer, var_read);
+	if (var_write == -1 || var_write != var_read)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+	free(buffer);
+	close(fd);
+	return(var_write);
+}
